136c9b97ad6a34845ce4e78d4ba6073a61a3abc2
spurs2k <spurlinjames@gmail.com>
2021-04-01 19:24:12 -0500

    Still working on section 4

--- queries.sql
+++ queries.sql
@@ -5,6 +5,11 @@
 SELECT NAME FROM employer where location = "Saint Louis"
 
 ## Part 3: Test it with SQL
-
+Drop Table job;
 
 ## Part 4: Test it with SQL
+SELECT * FROM skill
+LEFT JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id is not null
+ORDER BY name ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/JobData.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/JobData.java
@@ -12,12 +12,12 @@ public class JobData {
 
     /**
      * Returns the results of searching the Jobs data by field and search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column Job field that should be searched.
-     * @param value Value of the field to search for.
+     * @param column  Job field that should be searched.
+     * @param value   Value of the field to search for.
      * @param allJobs The list of jobs to search.
      * @return List of all jobs matching the criteria.
      */
@@ -25,11 +25,11 @@ public class JobData {
 
         ArrayList<Job> results = new ArrayList<>();
 
-        if (value.toLowerCase().equals("all")){
+        if (value.toLowerCase().equals("all")) {
             return (ArrayList<Job>) allJobs;
         }
 
-        if (column.equals("all")){
+        if (column.equals("all")) {
             results = findByValue(value, allJobs);
             return results;
         }
@@ -45,11 +45,11 @@ public class JobData {
         return results;
     }
 
-    public static String getFieldValue(Job job, String fieldName){
+    public static String getFieldValue(Job job, String fieldName) {
         String theValue;
-        if (fieldName.equals("name")){
+        if (fieldName.equals("name")) {
             theValue = job.getName();
-        } else if (fieldName.equals("employer")){
+        } else if (fieldName.equals("employer")) {
             theValue = job.getEmployer().toString();
         } else {
             theValue = job.getSkills().toString();
@@ -61,9 +61,9 @@ public class JobData {
     /**
      * Search all Job fields for the given term.
      *
-     * @param value The search term to look for.
+     * @param value   The search term to look for.
      * @param allJobs The list of jobs to search.
-     * @return      List of all jobs with at least one field containing the value.
+     * @return List of all jobs with at least one field containing the value.
      */
     public static ArrayList<Job> findByValue(String value, Iterable<Job> allJobs) {
         String lower_val = value.toLowerCase();
@@ -86,4 +86,6 @@ public class JobData {
 
         return results;
     }
+
+}
 
